9de6b36af338b6d625d5e738ab5e2db52da93495
Chris Schinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2025-12-15 09:26:59 +0100

    v2 -> v3

--- mux.go
+++ mux.go
@@ -8,23 +8,23 @@ import (
 	"net"
 	"time"
 
-	muxv2 "go.sia.tech/mux/v2"
+	muxv3 "go.sia.tech/mux/v3"
 )
 
 // A Mux multiplexes multiple duplex Streams onto a single net.Conn.
 type Mux struct {
-	m2 *muxv2.Mux
+	m3 *muxv3.Mux
 }
 
 // Close closes the underlying net.Conn.
 func (m *Mux) Close() error {
-	return m.m2.Close()
+	return m.m3.Close()
 }
 
 // AcceptStream waits for and returns the next peer-initiated Stream.
 func (m *Mux) AcceptStream() (*Stream, error) {
-	s, err := m.m2.AcceptStream()
-	return &Stream{s2: s}, err
+	s, err := m.m3.AcceptStream()
+	return &Stream{s3: s}, err
 }
 
 // DialStream creates a new Stream.
@@ -32,7 +32,7 @@ func (m *Mux) AcceptStream() (*Stream, error) {
 // Unlike e.g. net.Dial, this does not perform any I/O; the peer will not be
 // aware of the new Stream until Write is called.
 func (m *Mux) DialStream() *Stream {
-	return &Stream{s2: m.m2.DialStream()}
+	return &Stream{s3: m.m3.DialStream()}
 }
 
 // Dial initiates a mux protocol handshake on the provided conn.
@@ -49,8 +49,8 @@ func Dial(conn net.Conn, theirKey ed25519.PublicKey) (*Mux, error) {
 	if theirVersion[0] < 3 {
 		return nil, errors.New("versions 1 and 2 are no longer supported")
 	}
-	m, err := muxv2.Dial(conn, theirKey)
-	return &Mux{m2: m}, err
+	m, err := muxv3.Dial(conn, theirKey)
+	return &Mux{m3: m}, err
 }
 
 // Accept reciprocates a mux protocol handshake on the provided conn.
@@ -67,8 +67,8 @@ func Accept(conn net.Conn, ourKey ed25519.PrivateKey) (*Mux, error) {
 	if theirVersion[0] < 3 {
 		return nil, errors.New("versions 1 and 2 are no longer supported")
 	}
-	m, err := muxv2.Accept(conn, ourKey)
-	return &Mux{m2: m}, err
+	m, err := muxv3.Accept(conn, ourKey)
+	return &Mux{m3: m}, err
 }
 
 var anonPrivkey = ed25519.NewKeyFromSeed(make([]byte, 32))
@@ -87,17 +87,17 @@ func AcceptAnonymous(conn net.Conn) (*Mux, error) { return Accept(conn, anonPriv
 // A Stream is a duplex connection multiplexed over a net.Conn. It implements
 // the net.Conn interface.
 type Stream struct {
-	s2 *muxv2.Stream
+	s3 *muxv3.Stream
 }
 
 // LocalAddr returns the underlying connection's LocalAddr.
 func (s *Stream) LocalAddr() net.Addr {
-	return s.s2.LocalAddr()
+	return s.s3.LocalAddr()
 }
 
 // RemoteAddr returns the underlying connection's RemoteAddr.
 func (s *Stream) RemoteAddr() net.Addr {
-	return s.s2.RemoteAddr()
+	return s.s3.RemoteAddr()
 }
 
 // SetDeadline sets the read and write deadlines associated with the Stream. It
@@ -107,7 +107,7 @@ func (s *Stream) RemoteAddr() net.Addr {
 // setting a new deadline does not affect pending Read or Write calls, only
 // future calls.
 func (s *Stream) SetDeadline(t time.Time) error {
-	return s.s2.SetDeadline(t)
+	return s.s3.SetDeadline(t)
 }
 
 // SetReadDeadline sets the read deadline associated with the Stream.
@@ -115,7 +115,7 @@ func (s *Stream) SetDeadline(t time.Time) error {
 // This implementation does not entirely conform to the net.Conn interface:
 // setting a new deadline does not affect pending Read calls, only future calls.
 func (s *Stream) SetReadDeadline(t time.Time) error {
-	return s.s2.SetReadDeadline(t)
+	return s.s3.SetReadDeadline(t)
 }
 
 // SetWriteDeadline sets the write deadline associated with the Stream.
@@ -124,20 +124,20 @@ func (s *Stream) SetReadDeadline(t time.Time) error {
 // setting a new deadline does not affect pending Write calls, only future
 // calls.
 func (s *Stream) SetWriteDeadline(t time.Time) error {
-	return s.s2.SetWriteDeadline(t)
+	return s.s3.SetWriteDeadline(t)
 }
 
 // Read reads data from the Stream.
 func (s *Stream) Read(p []byte) (int, error) {
-	return s.s2.Read(p)
+	return s.s3.Read(p)
 }
 
 // Write writes data to the Stream.
 func (s *Stream) Write(p []byte) (int, error) {
-	return s.s2.Write(p)
+	return s.s3.Write(p)
 }
 
 // Close closes the Stream. The underlying connection is not closed.
 func (s *Stream) Close() error {
-	return s.s2.Close()
+	return s.s3.Close()
 }
