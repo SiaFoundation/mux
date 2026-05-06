9f2c0824975834e717a8e4c73940eb0f4354cb87
PJ <peterjan.brone@gmail.com>
2026-05-06 10:56:06 +0200

    mux: fix hanging close

--- v3/mux.go
+++ v3/mux.go
@@ -47,6 +47,10 @@ const (
 	// maxKeepalives is the maximum number of consecutive keepalives to send
 	// without any other traffic before closing the mux.
 	maxKeepalives = 4
+
+	// closeFlushTimeout is the maximum time Close will wait for pending
+	// writes to be flushed before tearing down the connection.
+	closeFlushTimeout = 5 * time.Second
 )
 
 // A Mux multiplexes multiple duplex Streams onto a single net.Conn.
@@ -357,12 +361,16 @@ func (m *Mux) readLoop() {
 
 // Close closes the underlying net.Conn.
 func (m *Mux) Close() error {
-	// if there's a buffered Write, wait for it to be sent
+	// wait briefly for pending writes to flush before tearing down
 	m.mu.Lock()
-	for len(m.writeBuf) != 0 && m.err == nil {
+	deadline := time.Now().Add(closeFlushTimeout)
+	timer := time.AfterFunc(closeFlushTimeout, m.bufferCond.Broadcast)
+	for len(m.writeBuf) != 0 && m.err == nil && time.Now().Before(deadline) {
 		m.bufferCond.Wait()
 	}
+	timer.Stop()
 	m.mu.Unlock()
+
 	err := m.setErr(ErrClosedConn)
 	if err == ErrClosedConn || err == ErrPeerClosedConn {
 		err = nil
